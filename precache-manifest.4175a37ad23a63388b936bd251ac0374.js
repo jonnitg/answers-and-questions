aa30aef3e600f2e0cbf6a2c4f2c3cde5e6162f5b
Jonatan Treviño Galindo <galtrev@gmail.com>
2020-04-07 22:27:35 -0700

    Updates

---
+++ precache-manifest.4175a37ad23a63388b936bd251ac0374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885e98563acc3711cd48ced6b1aeb508",
+    "url": "/answers-and-questions/index.html"
+  },
+  {
+    "revision": "4ccd1dfa1db7bbb7b9dd",
+    "url": "/answers-and-questions/static/js/2.a8933c07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/answers-and-questions/static/js/2.a8933c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c46c86be5fd39a71544",
+    "url": "/answers-and-questions/static/js/main.fe8a7350.chunk.js"
+  },
+  {
+    "revision": "ac8ef422f67735439734",
+    "url": "/answers-and-questions/static/js/runtime-main.b79a6c7c.js"
+  }
+]);
